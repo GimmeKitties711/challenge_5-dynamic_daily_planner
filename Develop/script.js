9253690d014a96460a6858535e31dc40771d5c8b
GimmeKitties711 <131449161+GimmeKitties711@users.noreply.github.com>
2023-07-31 16:52:57 -0700

    got the current date display in header to work

--- Develop/script.js
+++ Develop/script.js
@@ -15,12 +15,21 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  // source for getting current hour: https://day.js.org/docs/en/get-set/hour
   //
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  
+
   // TODO: Add code to display the current date in the header of the page.
+  var endings = ['st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th', 'th', 'st'];
+  // [1st, 2nd, 3rd, 4th, 5th, 6th, 7th, 8th, 9th, 10th, 11th, 12th, 13th, 14th, 15th, 16th, 17th, 18th, 19th 20th, 21st, 22nd, 23rd, 24th, 25th, 26th, 27th, 28th, 29th, 30th, 31st]
+  // source for correct endings for ordinal numbers: https://byjus.com/maths/ordinal-numbers/
+  var currentDay = $('#currentDay');
+  // source for jQuery selectors: https://www.w3schools.com/jquery/jquery_selectors.asp
+  currentDay.text(dayjs().format('dddd, MMMM DD') + endings[dayjs().date()-1]);
+  // source for date formatting options in dayjs: https://day.js.org/docs/en/display/format
+  // source for how to get current day of the month in dayjs: https://day.js.org/docs/en/get-set/date
 });
