fe4ac925943a8417c9768d583c5b84243f85e469
GimmeKitties711 <131449161+GimmeKitties711@users.noreply.github.com>
2023-08-02 18:46:26 -0700

    wrote local storage part, doesn't work though

--- Develop/script.js
+++ Develop/script.js
@@ -14,7 +14,18 @@ $(function () {
   // $(idArray[1]).children()[2].addEventListener("click", function () {console.log('woooo')});
   
   for (i=0; i<idArray.length; i++) {
-    $(idArray[i]).children()[2].addEventListener("click", function () {console.log('woooo')}); // come back to the function later
+    $(idArray[i]).children()[2].addEventListener("click", function () {
+      console.log($(this).siblings('.description').val());//.parent().attr('id')); // source for using '$(this)' instead of 'this' to enable jQuery functionality: https://www.geeksforgeeks.org/difference-between-this-and-this-in-jquery/
+      // source for getting the id of an element using jQuery: https://stackoverflow.com/questions/3239598/how-can-i-get-the-id-of-an-element-using-jquery
+
+      // localStorage.setItem("recent-image", imgSource);
+      // const recentImageSource = localStorage.getItem("recent-image");
+      // localStorage.setItem(idArray[0].split('#')[1], 'yooo')
+      localStorage.setItem(idArray[i], $(this).siblings('.description').val());
+      // source for findings an element's siblings: https://stackoverflow.com/questions/7463242/how-do-i-select-a-sibling-element-using-jquery
+      // source for using val() to get the current value from textarea: https://www.geeksforgeeks.org/how-to-get-the-value-of-a-textarea-in-jquery/
+
+    });
   }
   // source for the children() function in jQuery: https://www.digitalocean.com/community/tutorials/jquery-parent-and-children-tree-traversal-functions-example
 
